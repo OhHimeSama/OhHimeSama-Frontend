74165c13e3a898c4632580303036ca1375b767dc
wlgus123 <mail30019794@gmail.com>
2024-02-20 13:35:44 +0900

    :sparkles: Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <div className={styles["nav"]}>
+      <header className={styles["header"]}>
+        <div className={styles["nav-logo"]}>
+          <img src="../../img/logo.png" alt="logo" width={"130px"} />
+        </div>
+        <div className={styles["nav-user"]}>
+          <img src="../../img/user.png" alt="user" width={"25px"} />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
